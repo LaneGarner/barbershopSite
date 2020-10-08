aae0a307fe0d1a3d269c891a55cf1ef3483fd585
Lane Garner <b.lane.garner@gmail.com>
2020-10-08 16:12:54 -0500

    end of the day commit

--- scripts.js
+++ scripts.js
@@ -7,6 +7,50 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
 
 
 function displayMenu() {
-  const veggieBurgerMenu = document.getElementById("nav-menu");
-  veggieBurgerMenu.style.display = "flex";
+  document.querySelector("#nav-menu").classList.toggle("hide");
+  document.querySelector("#nav-menu").classList.toggle("show");
 }
+
+// function displayMenu() {
+//   const veggieBurgerMenu = document.getElementById("nav-menu");
+//   console.log('click');
+//   console.log(veggieBurgerMenu.style.display);
+
+//   if (veggieBurgerMenu.style.display = "none") {
+//     veggieBurgerMenu.style.display = "flex";
+//   } if (veggieBurgerMenu.style.display = "flex") {
+//     console.log("hide");
+//     veggieBurgerMenu.style.display = "none";
+//   } 
+// }
+
+// function displayMenu() {
+//   const veggieBurgerMenu = document.getElementById("nav-menu");
+//   console.log('click');
+//   // veggieBurgerMenu.style.display = "initial";
+//   console.log(veggieBurgerMenu.style.display);
+//   if (veggieBurgerMenu.style.display = "none") {
+//     veggieBurgerMenu.style.display = "flex";
+//   } else {
+//     console.log('hide');
+//     veggieBurgerMenu.removeAttribute("style");
+//     veggieBurgerMenu.style.display = "none";
+//   } 
+// }
+
+//   if (veggieBurgerMenu.style.display = "none") {
+//     veggieBurgerMenu.removeAttribute("style");
+//     veggieBurgerMenu.style.display = "none";
+//   } else {
+//     console.log('hide');
+//     veggieBurgerMenu.style.display = "flex";
+//   } 
+// }
+
+// function displayMenu() {
+//   const veggieBurgerMenu = document.getElementById("nav-menu");
+//   console.log('click');
+
+//   veggieBurgerMenu.style.display = "flex";
+    
+// }
