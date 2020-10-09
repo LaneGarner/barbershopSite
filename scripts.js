3668aafa9a80af16295e8f2184a6d4793a7b9a99
Lane Garner <b.lane.garner@gmail.com>
2020-10-08 21:25:34 -0500

    veggie burg getting closer

--- scripts.js
+++ scripts.js
@@ -5,52 +5,14 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   placeForResults.append(document.createElement("br"))
 })
 
-
 function displayMenu() {
-  document.querySelector("#nav-menu").classList.toggle("hide");
-  document.querySelector("#nav-menu").classList.toggle("show");
+  const veggieBurgerMenu = document.getElementById("nav-menu");
+  console.log('click');
+  console.log(veggieBurgerMenu.style.display);
+  
+  if (veggieBurgerMenu.style.display == "none") {
+  veggieBurgerMenu.style.display = "flex";
+  } else {
+    veggieBurgerMenu.style.display = "none";
+  }
 }
-
-// function displayMenu() {
-//   const veggieBurgerMenu = document.getElementById("nav-menu");
-//   console.log('click');
-//   console.log(veggieBurgerMenu.style.display);
-
-//   if (veggieBurgerMenu.style.display = "none") {
-//     veggieBurgerMenu.style.display = "flex";
-//   } if (veggieBurgerMenu.style.display = "flex") {
-//     console.log("hide");
-//     veggieBurgerMenu.style.display = "none";
-//   } 
-// }
-
-// function displayMenu() {
-//   const veggieBurgerMenu = document.getElementById("nav-menu");
-//   console.log('click');
-//   // veggieBurgerMenu.style.display = "initial";
-//   console.log(veggieBurgerMenu.style.display);
-//   if (veggieBurgerMenu.style.display = "none") {
-//     veggieBurgerMenu.style.display = "flex";
-//   } else {
-//     console.log('hide');
-//     veggieBurgerMenu.removeAttribute("style");
-//     veggieBurgerMenu.style.display = "none";
-//   } 
-// }
-
-//   if (veggieBurgerMenu.style.display = "none") {
-//     veggieBurgerMenu.removeAttribute("style");
-//     veggieBurgerMenu.style.display = "none";
-//   } else {
-//     console.log('hide');
-//     veggieBurgerMenu.style.display = "flex";
-//   } 
-// }
-
-// function displayMenu() {
-//   const veggieBurgerMenu = document.getElementById("nav-menu");
-//   console.log('click');
-
-//   veggieBurgerMenu.style.display = "flex";
-    
-// }
